7ee884294bee44aa736ad568cd52e7cd3d969c03
Fence-2 <Dima_2830@inbox.ru>
2020-03-27 22:10:38 +0300

    Внедрение зависимостей 2. Dependency Injection 2

--- src/main/java/ru/chistyakov/springlearning/TestSpring.java
+++ src/main/java/ru/chistyakov/springlearning/TestSpring.java
@@ -8,13 +8,13 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        //Внедрение зависимостей прошло успешно
-        //Music music = context.getBean("musicBean", Music.class);
-        //MusicPlayer musicPlayer = new MusicPlayer(music);
 
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
         musicPlayer.playMusic();
+
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
 
         context.close(); 
     }
