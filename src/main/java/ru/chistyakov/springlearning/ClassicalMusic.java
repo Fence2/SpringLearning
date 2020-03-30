0825ab9cc06539b04dc2fa7d028bf8d9322753ff
Fence-2 <Dima_2830@inbox.ru>
2020-03-30 17:20:53 +0300

    @PostConstruct and @PreDestroy

--- src/main/java/ru/chistyakov/springlearning/ClassicalMusic.java
+++ src/main/java/ru/chistyakov/springlearning/ClassicalMusic.java
@@ -3,21 +3,18 @@ package ru.chistyakov.springlearning;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
-//@Scope("singleton")
-@Scope("prototype")
 public class ClassicalMusic implements Music {
-    private ClassicalMusic() {
-    }
 
-    public static ClassicalMusic getClassicalMusic() {
-        return new ClassicalMusic();
-    }
-
+    @PostConstruct
     public void doMyInit() {
         System.out.println("Doing my initialization");
     }
 
+    @PreDestroy
     public void doMyDestroy() {
         System.out.println("Doing my destruction");
     }
--- src/main/java/ru/chistyakov/springlearning/TestSpring.java
+++ src/main/java/ru/chistyakov/springlearning/TestSpring.java
@@ -8,8 +8,6 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-//        Computer computer = context.getBean("computer", Computer.class);
-//        System.out.println(computer);
 
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
@@ -17,9 +15,6 @@ public class TestSpring {
         System.out.println(musicPlayer.getVolume());
 
         ClassicalMusic classicalMusic1 = context.getBean("classicalMusic", ClassicalMusic.class);
-        ClassicalMusic classicalMusic2 = context.getBean("classicalMusic", ClassicalMusic.class);
-
-        System.out.println(classicalMusic1 == classicalMusic2);
 
         context.close();
     }
